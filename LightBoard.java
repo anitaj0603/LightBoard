662dfa58793df291c33c9e6280ef457d601a6d58
anitaj0603 <anitaj10@nycstudents.net>
2025-05-06 00:28:53 +0000

    Finished

---
+++ LightBoard.java
@@ -0,0 +1,47 @@
+public class LightBoard {
+    private boolean[][] lights;
+
+    public LightBoard(int numRows, int numCols) {
+        lights = new boolean[numRows][numCols];
+        for (int r = 0; r < numRows; r++) {
+            for (int c = 0; c < numCols; c++) {
+                if (Math.random() < 0.4) {
+                    lights[r][c] = true;
+                }
+            }
+        }
+    }
+
+    public boolean evaluateLight(int row, int col) {
+        boolean light = lights[row][col];
+        int colLights = 0;
+        
+        for (int r = 0; r < lights.length; r++) {
+            if (lights[r][col]) {
+                colLights++;
+            }
+        }
+
+        if (light && colLights % 2 == 0) {
+            return false;
+        }
+        if (!light && colLights % 3 == 0) {
+            return true;
+        }
+        return light;
+    }
+
+    public void setLights(boolean[][] arr) {
+        lights = arr;
+    }
+
+    public double test() {
+        double on = 0;
+        for (boolean[] row : lights) {
+            for (boolean b : row) {
+                if (b) on++;
+            }
+        }
+        return on / (lights.length * lights[0].length);
+    }
+}
